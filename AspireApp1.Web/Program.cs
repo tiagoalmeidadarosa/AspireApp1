3dff83a363c411a0b643e67be2ff9369b2f4e8d1
Tiago Almeida da Rosa <tiago.almeida@poatek.com>
2023-12-22 17:58:33 -0300

    Add project files.

---
+++ AspireApp1.Web/Program.cs
@@ -0,0 +1,37 @@
+using AspireApp1.Web;
+using AspireApp1.Web.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add service defaults & Aspire components.
+builder.AddServiceDefaults();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddOutputCache();
+
+builder.Services.AddHttpClient<WeatherApiClient>(client=> client.BaseAddress = new("http://apiservice"));
+builder.Services.AddFluentUIComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+
+app.UseAntiforgery();
+
+app.UseOutputCache();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+    
+app.MapDefaultEndpoints();
+
+app.Run();
